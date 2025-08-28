9c2989c977752326901370a904409e7eaff60f01
Alex <alexhuamany@gmail.com>
2025-08-28 00:50:58 -0500

    FIX ci-cd

--- functions/src/config/firebase.ts
+++ functions/src/config/firebase.ts
@@ -12,9 +12,9 @@ if (!admin.apps.length) {
 
   admin.initializeApp({
     credential: admin.credential.cert({
-      projectId: FIREBASE_PROJECT_ID || "",
-      clientEmail: FIREBASE_CLIENT_EMAIL || "",
-      privateKey: FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n") || "",
+      projectId: FIREBASE_PROJECT_ID,
+      clientEmail: FIREBASE_CLIENT_EMAIL,
+      privateKey: FIREBASE_PRIVATE_KEY,
     }),
   });
 }
