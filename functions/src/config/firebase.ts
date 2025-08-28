6c0e703e54c7089f7a808e8c5021dd6a080bb5f9
Alex <alexhuamany@gmail.com>
2025-08-28 02:01:55 -0500

    FIX ci-cd

--- functions/src/config/firebase.ts
+++ functions/src/config/firebase.ts
@@ -1,16 +1,16 @@
 import admin from "firebase-admin";
 import * as dotenv from "dotenv";
-import { loadCredentials } from "./enviroment";
+// import { loadCredentials } from "./enviroment";
 
 dotenv.config();
-const { FIREBASE_PROJECT_ID, FIREBASE_CLIENT_EMAIL, FIREBASE_PRIVATE_KEY } = loadCredentials();
+// const { FIREBASE_PROJECT_ID, FIREBASE_CLIENT_EMAIL, FIREBASE_PRIVATE_KEY } = loadCredentials();
 
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      projectId: FIREBASE_PROJECT_ID || "",
-      clientEmail: FIREBASE_CLIENT_EMAIL || "",
-      privateKey: FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n") || "",
+      projectId: process.env.APP_FIREBASE_PROJECT_ID || "",
+      clientEmail: process.env.APP_FIREBASE_CLIENT_EMAIL || "",
+      privateKey: process.env.APP_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n") || "",
     }),
   });
 }
